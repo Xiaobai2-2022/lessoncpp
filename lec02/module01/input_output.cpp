47a90f86024a3f572cae42695e6d69a871f1c302
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2023-06-16 01:17:36 -0400

    Added notes to inputoutput

--- lec02/module01/input_output.cpp
+++ lec02/module01/input_output.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * Note that a whitespace(" ", "\n") is not the determinant factor for the compiler(g++), a semicolon(";") is however.
+ */
 int main() {
 
+    // declare variables
     string name;
     int age;
 
+    // cin is the standard input stream for c++, use ">>" to seperate cin and variable, variable and variable...
     cin >> name
         >> age;
 
+    // cout is the standard output stream for c++, use "<<" to seperate cout and variable, variable and variable...
     cout << "My name is " << name << ", I am " << age << " years old." << endl;
 
     return 0;
