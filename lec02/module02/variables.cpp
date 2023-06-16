c852b0562e63b485b0cb288d48b817014fa98fdf
33qaq <2936284027@qq.com>
2023-06-15 23:45:02 -0600

    edit variables

--- lec02/module02/variables.cpp
+++ lec02/module02/variables.cpp
@@ -3,9 +3,32 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World" << endl;
+    bool flag = false;
+    cout << "flag is: " << flag << endl;
 
-    cout << "Hello World\n";
+    char c = '0';
+    cout << "char equals: " << c << endl;
+
+    int val = 0;
+    cout << "value equals: " << val << endl;
+
+    long bigVal = 0L;
+    cout << "value equals: " << bigVal << endl;
+
+    float smallReal = 0.0;
+    cout << "real equals: " << smallReal << endl;
+
+    double real = 0.0;
+    cout << "real equals: " << real << endl;
+
+    string str = "message";
+    cout << "str is: " << str << endl;
+
+    unsigned int unsignedVal = 0;
+    cout << "value equals: " << unsignedVal << endl;
+
+    val = 1;
+    cout << "value equals: " << val << endl;
 
     return 0;
 
