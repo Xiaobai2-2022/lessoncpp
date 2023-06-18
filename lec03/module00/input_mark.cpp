cab470002e2d4222f8e955bb2a8b6815ae551d26
Xiaobai2-2022 <z2367li@uwaterloo.ca>
2023-06-17 21:57:45 -0400

    Added inputmark for test

---
+++ lec03/module00/input_mark.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// You MAY NOT modify this part of the code
+string prompt = "Please enter three different marks, seperated by a space: ";
+string output = "The average mark is: ";
+
+int main() {
+
+    /**
+     * For this assesment you are to ask the user to input a total of 3 real numbers,
+     *     seperated by a space, then you are to calculate the average of these 3 su-
+     *     bjects, and output the final value, end with a endline character.
+     *     Note that you must use the prompt and output given above.
+     * 
+     * Note that the formula for average of three number, a, b, c is:
+     *     (a + b + c) / 2
+     */
+    // Your implementation starts here:
+    
+
+    return 0;
+
+}
