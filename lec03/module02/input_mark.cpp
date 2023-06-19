d5e75a8270909fbd8c81293c5247d145cc906322
33qaq <2936284027@qq.com>
2023-06-18 20:52:15 -0600

    edit inputMark

--- lec03/module02/input_mark.cpp
+++ lec03/module02/input_mark.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 string prompt = "Please enter three different marks, seperated by a space: ";
 string output = "The average mark is: ";
-string pass = "The student passed with ";
+string pass = "The student passed.";
 string fail = "The student failed.";
 
 int main() {
 
     double sub1, sub2, sub3;
     double avg;
+    char grade = 'F';
 
     cout << prompt;
     cin >> sub1 >> sub2 >> sub3;
@@ -20,18 +21,33 @@ int main() {
 
     // if structure: if(condition) {statements} [else if {statements}] [else {statements}]
     if(avg >= 60) {
-        cout << pass;
+        cout << pass << endl;
         if(avg >= 90)
-            cout << "A." << endl;
+            grade = 'A';
         else if(avg >= 80)
-            cout << "B." << endl;
+            grade = 'B';
         else if(avg >= 70)
-            cout << "C." << endl;
+            grade = 'C';
         else 
-            cout << "D." << endl;
+            grade = 'D';
     }
-    else {
-        cout << fail << endl;
+    // else {
+    //     cout << fail << endl;
+    // }
+
+    switch(grade) {
+        case 'A':
+        case 'B':
+            cout << "Current standing: excellent." << endl;
+            break;
+        case 'C':
+            cout << "Current standing: good." << endl;
+            break;
+        case 'D':
+            cout << "Current standing: satisfactory." << endl;
+            break;
+        default:
+            cout << fail << endl;
     }
     
     return 0;
